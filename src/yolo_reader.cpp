603257fc461541c4b6f95517add311e3c027cb67
gwonhong <gwonhong.dev@gmail.com>
2021-09-14 18:00:48 +0900

    WIP not working right now

--- src/yolo_reader.cpp
+++ src/yolo_reader.cpp
@@ -1,11 +1,13 @@
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
 #include "ros/ros.h"
 #include "yolo_reader/BoundingBox.h"
 #include "yolo_reader/BoundingBoxes.h"
 
 void myCallback(const yolo_reader::BoundingBoxes::ConstPtr &msg) {
     std::vector<yolo_reader::BoundingBox> things = msg->bounding_boxes;
-    std::vector<yolo_reader::BoundingBox>::iterator thing;  //iterator
-    for (thing = things.begin(); thing != things.end(); thing++) {
+    for (std::vector<yolo_reader::BoundingBox>::iterator thing = things.begin(); thing != things.end(); thing++) {
         int64_t x = (thing->xmin + thing->xmax) / 2;
         int64_t y = (thing->ymin + thing->ymax) / 2;
         ROS_INFO("name: %s && x: [%ld] && y: [%ld]\n", thing->Class.c_str(), x, y);
@@ -17,6 +19,9 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "yolo_reader");
 
     ros::NodeHandle n;
+
+    message_filters::Subscriber<BoundingBoxes> detector(n, "detector", 1000);
+    message_filters::Subscriber;
 
     ros::Subscriber sub = n.subscribe("/darknet_ros/bounding_boxes", 1000, myCallback);
 
