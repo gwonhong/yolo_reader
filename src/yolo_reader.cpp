5c3817903a8da7b59da8cb59d3f10df472241bad
gwonhong <gwonhong.dev@gmail.com>
2021-09-26 10:20:32 +0900

    not working

--- src/yolo_reader.cpp
+++ src/yolo_reader.cpp
@@ -1,45 +1,35 @@
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
-#include <sensor_msgs/PointCloud.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/point_cloud_conversion.h>
-
+#include "librealsense2/rs.hpp"
 #include "ros/ros.h"
 #include "yolo_reader/BoundingBox.h"
 #include "yolo_reader/BoundingBoxes.h"
 
-sensor_msgs::PointCloud pointcloud;
+rs2::pipeline p;
+
+p.start();
 
 void YOLO_Callback(const yolo_reader::BoundingBoxes::ConstPtr &msg) {
     std::vector<yolo_reader::BoundingBox> things = msg->bounding_boxes;
     for (std::vector<yolo_reader::BoundingBox>::iterator thing = things.begin(); thing != things.end(); thing++) {
         if (thing->Class == "bottle") {
             ROS_INFO("@@bottle detected@@");
+            // Block program until frames arrive
+            rs2::frameset frames = p.wait_for_frames();
+
+            // Try to get a frame of a depth image
+            rs2::depth_frame depth = frames.get_depth_frame();
+
             int64_t x = (thing->xmin + thing->xmax) / 2;
             int64_t y = (thing->ymin + thing->ymax) / 2;
-            ROS_INFO("x: [%ld] && y: [%ld]\n", x, y);
 
-            for (int i = 0; i < pointcloud.points.size(); i++) {  //다 도는게 너무 오래걸림
-                // ROS_INFO("%d/%d is read from clouds", i, pointcloud.points.size());  //모든 점을 다 도는지 개수로 확인
-                float dx = pointcloud.points[i].x - (float)x;
-                float dy = pointcloud.points[i].y - (float)y;
-                // ROS_INFO("%f, %f away from detected point\n", dx, dy);  //확실히 coordinate가 다름.
-                if (dx < 0) dx = -dx;
-                if (dy < 0) dy = -dy;
-                if (dx < 1 && dy < 1) {
-                    float distance = pointcloud.points[i].z;
-                    ROS_INFO("@!@ the distance is [%f]\n", distance);
-                    break;
-                }
-            }
+            // Query the distance from the camera
+            float detected_depth = depth.get_distance(x, y);
+
+            ROS_INFO("x: [%ld] && y: [%ld] && depth: [%f]\n", x, y, detected_depth);
+
             ROS_INFO("DONE!\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         }
     }
     ROS_INFO("\n");
-}
-
-void distance_sensor_Callback(const sensor_msgs::PointCloud2::ConstPtr &msg) {
-    sensor_msgs::convertPointCloud2ToPointCloud(*msg, pointcloud);
 }
 
 int main(int argc, char **argv) {
@@ -48,7 +38,6 @@ int main(int argc, char **argv) {
     ros::NodeHandle n;
 
     ros::Subscriber image_sub = n.subscribe("/darknet_ros/bounding_boxes", 100, YOLO_Callback);
-    ros::Subscriber depth_sub = n.subscribe("/camera/depth/color/points", 100, distance_sensor_Callback);
 
     ros::spin();
 
